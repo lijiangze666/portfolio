9a764e9081b3c25de97702cdf8ade9ca07c20237
leejz <957675793@qq.com>
2024-10-30 13:00:13 +0800

    update bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type {Metadata} from "next";
+// import type {Metadata} from "next";
 import "./globals.css";
 import {ThemeProvider} from "@/app/providers";
 import {Open_Sans} from "next/font/google";
+import Head from "next/head";
 const open = Open_Sans({subsets: ["latin"], weight: ["400", "700"]});
 
-export const metadata: Metadata = {
-    title: "Leejz's Portfolio",
-    description: "Leejz's Portfolio",
-};
+// export const metadata: Metadata = {
+//     title: "Leejz's Portfolio",
+//     description: "Leejz's Portfolio",
+// };
 
 export default function RootLayout({
                                        children,
@@ -15,11 +16,12 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
-        <head>
+        <html lang="en">
+        <Head>
+            <title>Leejz&rsquo;s Portfolio</title>
+            <meta name="description" content="Leejz's Portfolio"/>
             <link rel="icon" href="/logo.png" sizes="any"/>
-            <title></title>
-        </head>
+        </Head>
         <body className={open.className}>
         <ThemeProvider
             attribute="class"
